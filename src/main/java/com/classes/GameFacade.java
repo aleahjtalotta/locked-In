3e4eaf06ef27dc9bfff89954786bd9b12e3b65de
aleahjtalotta <atalotta47@gmail.com>
2025-10-07 12:11:24 -0400

    created method stubs for game facade

--- src/main/java/com/classes/GameFacade.java
+++ src/main/java/com/classes/GameFacade.java
@@ -1,5 +1,38 @@
 package com.classes;
 
 public class GameFacade {
-    
+    private GameSystem gameSystem;
+    private DataLoader dataLoader;
+    private DataWriter dataWriter;
 }
+
+public void startNewGame() {
+    //
+}
+
+public void loadGame() {
+    //
+}
+
+public void saveGame() {
+    //
+}
+
+public void submitAnswer() {
+    //
+}
+
+public void showLeaderboard() {
+    //
+}
+
+public void loginPlayer() {
+    //
+}
+
+public void logoutPlayer() {
+    //
+}
+
+
+
