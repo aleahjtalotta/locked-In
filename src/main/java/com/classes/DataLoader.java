fd534a616f547ade63abf8d8a0635759cf1d85d2
anchalpatel05 <anchalp055@gmail.com>
2025-10-08 13:50:11 -0400

    Added the stubs for DataLoader and DataWriter

--- src/main/java/com/classes/DataLoader.java
+++ src/main/java/com/classes/DataLoader.java
@@ -1,5 +1,31 @@
 package com.classes;
 
 public class DataLoader {
+    private String source;
+
+    public DataLoader(String source) {
+        this.source = source;
+    }
+
+    public GameSystem loadGame(){
+        return null;
+    }
+
+    public PlayerList loadPlayers(){
+        return null;
+    }
+
+    public PuzzleList loadPuzzles() {
+        return null;
+    }
+    
+    public RoomList loadRooms(){
+        return null;
+    }
+
+    public ItemList loadItems(){
+        return null;
+    }
+
     
 }
--- src/main/java/com/classes/DataWriter.java
+++ src/main/java/com/classes/DataWriter.java
@@ -1,5 +1,21 @@
 package com.classes;
 
 public class DataWriter {
-    
+    private String destination;
+
+    public DataWriter(String destination) {
+        this.destination = destination;
+    }
+
+    public void savePlayers(PlayerList pl){
+
+     }
+
+     public void saveLeaders(Leaderboard lb){
+
+     }
+
+     public void saveGame(GameSystem gs){
+
+     }
 }
