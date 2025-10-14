c1cf5aa78814466f3cc56f0bcb2e05db45a309d1
aleahjtalotta <atalotta47@gmail.com>
2025-10-14 12:41:46 -0400

    added stubs for GameSystem

--- src/main/java/com/classes/GameSystem.java
+++ src/main/java/com/classes/GameSystem.java
@@ -1,5 +1,27 @@
 package com.classes;
 
 public class GameSystem {
-    
+    private RoomList rooms;
+    private PuzzleList puzzles;
+    private Playerlist players;
+    private Leaderboard leaderboard;
+    private Hints hints;
+    private Timer timer;
+    private DifficultyLevel Currentdifficulty;
+
+    public void startGame() {
+        //
+    }
+
+    public void quitGame() {
+        //
+    }
+
+    public void restartGame() {
+        //
+    }
+
+    public void setDifficulty(DifficultyLevel level) {
+        //
+    }
 }
