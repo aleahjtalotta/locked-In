f6d34d56ae17c68281096e433700a7ce877a2d22
JasmineLee7 <jgl4@email.sc.edu>
2025-10-15 17:08:29 -0400

    added a timer class

---
+++ src/main/java/com/classes/Timer.java
@@ -0,0 +1,19 @@
+package com.classes;
+
+public class Timer {
+    
+
+    public void startTimer(){
+        
+    }
+
+    public void pauseTimer(){
+        
+    }
+
+    public void resetTimer(){
+        
+    }
+
+    
+}
