dbf5493d7196165ca016d26993f5949dd995df64
JasmineLee7 <jgl4@email.sc.edu>
2025-10-15 16:51:06 -0400

    playerlist

--- src/main/java/com/classes/PuzzleList.java
+++ src/main/java/com/classes/PuzzleList.java
@@ -1,5 +1,24 @@
 package com.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PuzzleList {
+    private List<Player> players;
     
+    public PlayerList() {
+        players = new ArrayList();
+    }
+
+    public void addPlayer(Player p) {
+        players.add(p);
+    }
+
+    public void removePlayer(Player p) {
+        players.remove(p);
+    }
+
+    public Player getPlayer(int index) {
+        return players.get(index);
+    }
 }
