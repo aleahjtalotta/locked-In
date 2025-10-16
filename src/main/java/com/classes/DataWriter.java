69bff6b097c8bb301f1968c83d5368178d0b9db9
Firstname Lastname <youremail@domain.com>
2025-10-16 13:46:07 -0400

    leaderboard DataWriter

--- src/main/java/com/classes/DataWriter.java
+++ src/main/java/com/classes/DataWriter.java
@@ -1,5 +1,7 @@
 package com.classes;
 
+import java.io.FileWriter;
+
 public class DataWriter {
     private String destination;
 
@@ -13,7 +15,12 @@ public class DataWriter {
      }
 
      public void saveLeaders(Leaderboard lb){
-
+        try(FileWriter writer = new FileWriter(destination + "leaderboard.txt", false)){
+            for(int i = 0; i < lb.getSize(); i++){
+                writer.write(lb.getEntry(i).getName() + "," + lb.getEntry(i).getScore() + "\n");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
      }
 
      public void saveGame(GameSystem gs){
