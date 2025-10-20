d9934410c14a3088f21c207c94a14fd168a2f8fa
s-kottapali <skottapali1325@gmail.com>
2025-10-20 14:58:59 -0400

    WriteInPuzzle Implementation

--- src/main/java/com/classes/WriteInPuzzle.java
+++ src/main/java/com/classes/WriteInPuzzle.java
@@ -1,5 +1,23 @@
 package com.classes;
 
-public class WriteInPuzzle {
-    
+import java.util.Objects;
+import java.util.UUID;
+
+public class WriteInPuzzle extends Puzzle {
+    private final String correctAnswer;
+
+    public WriteInPuzzle(UUID id, Long legacyId, String name, String description, String reward,
+                         String correctAnswer, boolean solved) {
+        super(id, legacyId, name, description, reward, PuzzleType.WRITE_IN, solved);
+        this.correctAnswer = Objects.requireNonNullElse(correctAnswer, "");
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    @Override
+    public boolean isCorrectAnswer(String answer) {
+        return correctAnswer.equalsIgnoreCase(Objects.requireNonNullElse(answer, ""));
+    }
 }
