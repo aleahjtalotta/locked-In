db785fb2a094c58f88f295d2c11ac962fe14aaeb
aleahjtalotta <atalotta47@gmail.com>
2025-10-20 12:39:23 -0400

    filled in stubs and completed leaderboard class

--- src/main/java/com/classes/Leaderboard.java
+++ src/main/java/com/classes/Leaderboard.java
@@ -1,5 +1,8 @@
 package com.classes;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Leaderboard {
@@ -9,4 +12,47 @@ public class Leaderboard {
     public Leaderboard() {
         scores = new ArrayList<>();
     }
-}
+
+    public void updateLeaderboard(Player player, int score) {
+
+        ScoreEntry found = null;
+        for (ScoreEntry entry : scores) {
+            if (entry.getPlayerName().equals(player.getName())) {
+                found = entry;
+                break;
+                }
+            }
+            if (found != null) {
+                if (score > found.getScore()) {
+                    scores.remove(found);
+                    scores.add(new ScoreEntry(player.getName(), score, null));
+
+                }
+            }else {
+                scores.add(new ScoreEntry(player.getName(), score, null));
+
+            }
+            
+            sortLeaderboard();
+        }
+            public void displayTopPlayer() {
+                if (scores.isEmpty()) {
+                    System.out.println("No scores available yet!");
+                    return;
+                }
+                ScoreEntry top = scores.get(0);
+                System.out.println("TOP PLAYER: " + top.getPlayerName() + " - " + top.getScore());
+            }
+            public void displayPlayerScore(Player player) {
+                for (ScoreEntry entry : scores) {
+                    if (entry.getPlayerName().equals(player.getName())) {
+                        System.out.println(player.getName() + "'s score: " + entry.getScore());
+                        return;
+                    }
+                }
+                System.out.println(player.getName() + " has no score on the leaderboard.");
+            } 
+            private void sortLeaderboard() {
+                Collections.sort(scores, Comparator.comparingInt(ScoreEntry::getScore).reversed());
+            }
+        }
