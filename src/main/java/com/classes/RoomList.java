5b98c245cf96e983ead4b872649dab21f1592400
s-kottapali <skottapali1325@gmail.com>
2025-10-20 14:58:15 -0400

    Room List implementation

--- src/main/java/com/classes/RoomList.java
+++ src/main/java/com/classes/RoomList.java
@@ -1,5 +1,33 @@
 package com.classes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public class RoomList {
-    
+    private final List<Room> rooms;
+
+    public RoomList() {
+        this.rooms = new ArrayList<>();
+    }
+
+    public void add(Room room) {
+        if (room != null) {
+            rooms.add(room);
+        }
+    }
+
+    public List<Room> asList() {
+        return Collections.unmodifiableList(rooms);
+    }
+
+    public Optional<Room> findById(UUID id) {
+        return rooms.stream().filter(r -> r.getId().equals(id)).findFirst();
+    }
+
+    public int size() {
+        return rooms.size();
+    }
 }
