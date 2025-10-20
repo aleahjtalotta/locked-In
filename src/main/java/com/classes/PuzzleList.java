1f46a838289e1884b774098b833c92c1c0905f79
s-kottapali <skottapali1325@gmail.com>
2025-10-20 14:57:08 -0400

    Puzzle List Methods

--- src/main/java/com/classes/PuzzleList.java
+++ src/main/java/com/classes/PuzzleList.java
@@ -1,5 +1,33 @@
 package com.classes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public class PuzzleList {
-    
+    private final List<Puzzle> puzzles;
+
+    public PuzzleList() {
+        this.puzzles = new ArrayList<>();
+    }
+
+    public void add(Puzzle puzzle) {
+        if (puzzle != null) {
+            puzzles.add(puzzle);
+        }
+    }
+
+    public List<Puzzle> asList() {
+        return Collections.unmodifiableList(puzzles);
+    }
+
+    public Optional<Puzzle> findById(UUID id) {
+        return puzzles.stream().filter(p -> p.getId().equals(id)).findFirst();
+    }
+
+    public int size() {
+        return puzzles.size();
+    }
 }
