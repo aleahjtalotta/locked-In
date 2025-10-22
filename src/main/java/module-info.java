19571bf01f6ad61c187146be830a810759031cc3
JasmineLee7 <jgl4@email.sc.edu>
2025-10-22 16:26:54 -0400

    not sure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,5 @@ module com.ourgroup1 {
     opens com.ourgroup1 to javafx.fxml;
     exports com.ourgroup1;
 
-    opens com.model to javafx.fxml;
-    exports com.model;
+    exports com.classes;
 }
