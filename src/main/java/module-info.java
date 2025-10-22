b0599615ee5b16844238a8d0f8bee4e54d60d6df
JasmineLee7 <jgl4@email.sc.edu>
2025-10-22 16:48:58 -0400

    fixed something in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,5 +9,10 @@ module com.ourgroup1 {
     // for the sake of testing this is commented out temperarily
     //opens com.model to javafx.fxml;
     //exports com.model;
+
+
+    // changed so it runs
+    opens com.classes to javafx.fxml;
     exports com.classes;
+
 }
