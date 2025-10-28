4d35b4e614b9f036bbebf101fa315d95911cc859
JasmineLee7 <jgl4@email.sc.edu>
2025-10-28 14:17:08 -0400

    Add testing folder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,15 @@
 module com.lockedin {
     requires javafx.controls;
+    //requires javafx.fxml;
     requires json.simple;
+    requires junit;
+
+
 
     exports com.classes;
     exports com.lockedin.ui;
+
+
+    //opens com.classes to javafx.fxml;
+   // exports com.classes;
 }
---
+++ src/test/java/com/classes/TestTesting.java
@@ -0,0 +1,11 @@
+package com.classes;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestTesting {
+    @Test
+    public void testTesting(){
+        assertTrue(true);
+    }
+}
